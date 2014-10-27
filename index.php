f0b332f3f0a2bba1ec64d575c9e35b9b3744e724
Merili <meriliasu@gmail.com>
2014-10-27 10:27:16 +0200

    Initial commit
    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>2. periood 8. praktikum</title>
+</head>
+<body>
+	<?php
+	  $name = "test";
+	  $value = "45";
+	  $expire = "";
+	  setcookie($name, $value, $expire);
+	  // nimetus, väärtus, aegumishetk
+	?>
+</body>
+</html>
+	
